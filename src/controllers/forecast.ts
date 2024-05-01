35d2121b44abdfe18a41e09bb8081ecc44127e70
OpKSpatY <mcpedrohenriquelc@gmail.com>
2024-05-01 15:04:11 -0300

    feat: middlewares + jwt

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,24 +1,27 @@
-import { Controller, Get } from '@overnightjs/core';
+import { Controller, Get, ClassMiddleware } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { Beach } from '@src/models/beach';
 import { Forecast } from '@src/services/forecast';
+import { authMiddleware } from '@src/middlewares/auth';
+import logger from '@src/logger';
+import { BaseController } from '.';
 
 const forecast = new Forecast();
 
 @Controller('forecast')
-export class ForecastController {
+@ClassMiddleware(authMiddleware)
+export class ForecastController extends BaseController{
   @Get('')
   public async getForecastForgeLoggedUser(
-    _: Request,
+    req: Request,
     res: Response
   ): Promise<void> {
     try {
-      const beaches = await Beach.find({});
+      const beaches = await Beach.find({ user: req.decoded?.id });
       const forecastData = await forecast.processForecastForBeaches(beaches);
       res.status(200).send(forecastData);
     } catch (error) {
-      console.error((error as Error))
-      res.status(500).send({ error: 'Something went wrong' });
+      this.sendErrorResponse(res, {code: 500, message: 'Something went wrong'})
     }
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,17 @@ import './util/module-alias';
 import { Server } from '@overnightjs/core';
 import { Application } from 'express';
 import bodyParser from 'body-parser';
+import * as http from 'http';
+import expressPino from 'express-pino-logger';
+import cors from 'cors';
 import { ForecastController } from './controllers/forecast';
 import * as database from '@src/database';
 import { BeachesController } from './controllers/beaches';
 import { UsersController } from './controllers/users';
+import logger from './logger';
 
 export class SetupServer extends Server {
+  private server?: http.Server;
   /*
    * same as this.port = port, declaring as private here will
    * add the port variable to the SetupServer instance
@@ -28,7 +33,16 @@ export class SetupServer extends Server {
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
-    this.setupControllers();
+    this.app.use(
+      expressPino({
+        logger,
+      })
+    );
+    this.app.use(
+      cors({
+        origin: '*',
+      })
+    );
   }
 
   private setupControllers(): void {
@@ -52,11 +66,21 @@ export class SetupServer extends Server {
 
   public async close(): Promise<void> {
     await database.close();
+    if (this.server) {
+      await new Promise((resolve, reject) => {
+        this.server?.close((err) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(true);
+        });
+      });
+    }
   }
 
   public start(): void {
-    this.app.listen(this.port, () => {
-      console.info('Server listening on port: ' + this.port);
+    this.server = this.app.listen(this.port, () => {
+      logger.info('Server listening on port: ' + this.port);
     });
   }
 }
